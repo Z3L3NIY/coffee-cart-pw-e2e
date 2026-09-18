c485a2a213417e554f5a0b66e0b2733084c7dcba
Serhii Husenko <sgusenko@gmail.com>
2026-09-18 20:05:24 +0300

    Refactor Playwright configuration and add new test cases for coffee cart functionality

---
+++ tests/coffee-cart/coffee-cart.spec.ts
@@ -0,0 +1,74 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+    await page.goto("/");
+});
+
+test("Cart counter is 0 by default", async ({ page }) => {
+    await expect(page.locator("a[href='/cart']")).toBeVisible();
+    await expect(page.locator("a[href='/cart']")).toContainText("cart (0)");
+});
+
+test("Empty cart page has default text placeholder", async ({ page }) => {
+    await page.locator("a[href='/cart']").click();
+    await expect(page.locator(".list > p")).toBeVisible();
+    await expect(page.locator(".list > p")).toHaveText(
+        "No coffee, go add some.",
+    );
+});
+
+test("Adding a drink updates the cart counter", async ({ page }) => {
+    await page.locator('[data-test="Espresso"]').click();
+    await expect(page.locator("a[href='/cart']")).toHaveText("cart (1)");
+});
+
+test("Total is calculated correctly for multiple drinks", async ({ page }) => {
+    await page.locator('[data-test="Espresso"]').click();
+    await page.locator('[data-test="Espresso_Macchiato"]').click();
+    await expect(page.locator("a[href='/cart']")).toHaveText("cart (2)");
+    await expect(page.locator('[data-test="checkout"]')).toHaveText(
+        "Total: $22.00",
+    );
+});
+
+test("Added drinks are shown on the cart page", async ({ page }) => {
+    await page.locator('[data-test="Espresso"]').click();
+    await page.locator('[data-test="Cappuccino"]').click();
+    await page.locator("a[href='/cart']").click();
+    await expect(
+        page.locator(".list-item > div").filter({ hasText: /^Espresso$/ }),
+    ).toBeVisible();
+    await expect(
+        page.locator(".list-item > div").filter({ hasText: /^Cappuccino$/ }),
+    ).toBeVisible();
+});
+
+test("Promo proposal is visible", async ({ page }) => {
+    await page.locator('[data-test="Espresso"]').click();
+    await page.locator('[data-test="Espresso_Macchiato"]').click();
+    await page.locator('[data-test="Cappuccino"]').click();
+    await expect(page.locator(".promo span")).toBeVisible();
+    await expect(page.locator(".promo span")).toHaveText(
+        "It's your lucky day! Get an extra cup of Mocha for $4.",
+    );
+});
+
+test("Payment fields are editable", async ({ page }) => {
+    await page.locator('[data-test="checkout"]').click();
+    await page.locator("#name").fill("Serhii");
+    await expect(page.locator("#name")).toHaveValue("Serhii");
+    await page.locator("#email").fill("test@test.com");
+    await expect(page.locator("#email")).toHaveValue("test@test.com");
+});
+
+test("Successful payment message is shown", async ({ page }) => {
+    await page.locator('[data-test="Espresso"]').click();
+    await page.locator('[data-test="checkout"]').click();
+    await page.locator("#name").fill("Serhii");
+    await page.locator("#email").fill("test@test.com");
+    await page.locator("#submit-payment").click();
+    await expect(page.locator(".snackbar.success")).toBeVisible();
+    await expect(page.locator(".snackbar.success")).toHaveText(
+        "Thanks for your purchase. Please check your email for payment.",
+    );
+});
